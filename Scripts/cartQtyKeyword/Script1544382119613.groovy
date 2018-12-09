6384e43627c792c7457e9a0fc73b529813abfb2c
Adewale <adetutorial@gmail.com>
2018-12-09 19:12:50 +0000

    update keyword test case

---
+++ Scripts/cartQtyKeyword/Script1544382119613.groovy
@@ -0,0 +1,31 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.did.ie/')
+
+CustomKeywords.'didcartQty.cartQty.searchForItemAndAddToCart'('iphone 7')
+
+WebUI.setText(findTestObject('Page_Qty/input_qty'), '2')
+
+WebUI.click(findTestObject('Page_Qty/button_'))
+
+WebUI.click(findTestObject('Page_Qty/td_1299.98'))
+
+WebUI.verifyElementText(findTestObject('Page_Qty/td_1299.98'), '€1,299.98')
+
+WebUI.closeBrowser()
+
